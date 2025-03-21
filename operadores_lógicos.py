0f3b9b36e1876101b23d50bedc34301bfcb9f63d
Johnson <68112110+herijohnson@users.noreply.github.com>
2025-03-21 12:20:07 -0300

    Update operadores_lógicos.py
    exercício de operadores lógicos

--- operadores_lógicos.py
+++ operadores_lógicos.py
@@ -1,1 +1,17 @@
+# And = Para ser True todos tem que ser True
+# Or = Para ser True pelo menos deve ser True
 
+print(True and True)
+print(True and False)
+print(False and False)
+print(True or False)
+print(True or False)
+print(False or False)
+
+saldo = 1000
+saque = 250
+limite = 200
+conta_especial = True
+
+exp = (saldo >= saque and saque and saque <= limite) or (conta_especial and saldo >= saque)
+print(exp)
